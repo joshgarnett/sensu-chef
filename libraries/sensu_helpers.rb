cce3780ca0d7e2ebfe5d63ae97ef9cade078dcb7
Joshua Garnett <jgarnett@zynga.com>
2018-08-02 08:03:30 -0400

    Improve amazon 2 check

--- libraries/sensu_helpers.rb
+++ libraries/sensu_helpers.rb
@@ -152,7 +152,7 @@ module Sensu
       # @param platform_version [String] The platform version, as reported by ohai
       def amazon_linux_2_rhel_version(platform_version)
         return "6" if /201\d/.match?(platform_version)
-        return "7" if platform_version == "2"
+        return "7" if platform_version == "2" || platform_version.include?("amzn2")
         raise "Unsupported Linux platform version #{platform_version} - rhel version unknown"
       end
 
